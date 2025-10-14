8119de4391c18678fc22faabd781cb24ac3147d7
HStiven <hernandostivencortes@gmail.com>
2025-10-13 22:35:00 -0500

    Primer commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-
+import TableOperator from "./table/table-operator"
 
 function App() {
 
   return (
-    <section className='section-body-main'>
-      <article className="w-100 d-flex flex-column rounded" style={{background: '#282a2c'}}>
-
-      </article>
+    <section className='w-100' style={{ padding: '20px 40px' }}>
+      <header className="w-100 d-flex flex-column align-items-center">
+        <div className="d-flex justify-content-center">
+          <h1 className="text-white">Tabla de verdad</h1>
+        </div>
+        <p className="w-50 text-center mb-0" style={{fontSize: '0.8rem', color: '#ffffff94'}}>Generador interactivo de tablas de verdad para operadores lógicos</p>
+      </header>
+      <TableOperator/>
     </section>
   )
 }
---
+++ src/table/table-operator.tsx
@@ -0,0 +1,11 @@
+const TableOperator = () => {
+    return (
+        <main className="w-100 d-flex flex-column">
+            <div className="w-100 d-flex flex-column" style={{ background: '#282a2c', borderRadius: '10px' }}>
+                
+            </div>
+        </main>
+    )
+}
+
+export default TableOperator;
