b07258b38af7aaf56369d83b4aa67379bf6164a0
HStiven <hernandostivencortes@gmail.com>
2025-10-13 21:47:12 -0500

    Proyecto tablas de verdad

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <section className='section-body-main'>
+      <article className="w-100 d-flex flex-column rounded" style={{background: '#282a2c'}}>
+
+      </article>
+    </section>
+  )
+}
+
+export default App
